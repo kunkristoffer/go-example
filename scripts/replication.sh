f5049182b317229cab7cdd5aad934f9c41d4a066
kristoffer <kunkristoffer@gmail.com>
2025-06-07 19:20:42 +0200

    adding replica url back to cmd for testing on fly.io

--- scripts/replication.sh
+++ scripts/replication.sh
@@ -6,7 +6,7 @@ if [ -f /database/chat.db ]; then
 	echo "Database already exists, skipping restore"
 else
 	echo "No database found, restoring from replica (${REPLICA_URL}) if exists"
-	litestream restore -if-replica-exists -o database/chat.db
+	litestream restore -if-replica-exists -o database/chat.db "${REPLICA_URL}"
 fi
 
 # Run litestream with your app as the subprocess.
