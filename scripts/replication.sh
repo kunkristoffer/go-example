f7d9211448860fcd4ca1b889ef74b124cba453e7
kristoffer <kunkristoffer@gmail.com>
2025-06-07 16:33:02 +0200

    fixing pathing formatting error in script

--- scripts/replication.sh
+++ scripts/replication.sh
@@ -5,9 +5,9 @@ set -e
 if [ -f /database/chat.db ]; then
 	echo "Database already exists, skipping restore"
 else
-	echo "No database found, restoring from replica if exists"
-	litestream restore -v -if-replica-exists -o /database/chat.db "${REPLICA_URL}"
+	echo "No database found, restoring from replica (${REPLICA_URL}) if exists"
+	litestream restore -if-replica-exists -o database/chat.db "${REPLICA_URL}"
 fi
 
 # Run litestream with your app as the subprocess.
-exec litestream replicate -exec "/usr/local/bin/chat-app -dsn /database/chat.db"
+exec litestream replicate -exec "/usr/local/bin/chat-app -dsn database/chat.db"
