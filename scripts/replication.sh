b570d794d6dff2df8398f4e9452c3acf36104d69
kristoffer <kunkristoffer@gmail.com>
2025-06-07 12:54:44 +0200

    created shell script for replicating db on startup if none detected

---
+++ scripts/replication.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Restore the database if it does not already exist.
+if [ -f /database/chat.db ]; then
+	echo "Database already exists, skipping restore"
+else
+	echo "No database found, restoring from replica if exists"
+	litestream restore -v -if-replica-exists -o /database/chat.db "${REPLICA_URL}"
+fi
+
+# Run litestream with your app as the subprocess.
+exec litestream replicate -exec "/usr/local/bin/myapp -dsn /database/chat.db"
