9b5b58e6480bebe01fbad74d218c6caf9defeb69
kristoffer <kunkristoffer@gmail.com>
2025-06-08 12:49:50 +0200

    Modified litestream to post warning on missing config file and updated restore commands as args was causing problems

--- scripts/replication.sh
+++ scripts/replication.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 set -e
 
+# Validate config file
+if [ -f /etc/litestream.yml ]; then
+	echo "Configuration file found"
+else
+	echo "No configuration found, using cli arguments"
+fi
+
 # Restore the database if it does not already exist.
 if [ -f /database/chat.db ]; then
 	echo "Database already exists, skipping restore"
 else
-	echo "No database found, restoring from replica (${REPLICA_URL}) if exists"
-	litestream restore -if-replica-exists -o database/chat.db "${REPLICA_URL}"
+	echo "No database found, restoring from replica if exists"
+	litestream restore /database/chat.db
 fi
 
 # Run litestream with your app as the subprocess.
